9d884c342929804385dd52e15e805d78089e8ded
saadhossain <mshsarkar@gmail.com>
2022-10-17 12:36:46 +0600

    Footer responsive optimized

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,12 +3,12 @@ import React from 'react';
 const Footer = () => {
     return (
         <div className='bg-[#181A1B] py-5 mt-10'>
-            <div className='w-10/12 mx-auto'>
-                <section className='flex justify-between gap-10'>
+            <div className='md:w-10/12 mx-auto'>
+                <section className='md:flex justify-between gap-5 md:gap-10 ml-10 md:ml-0'>
                     {/* Compnay information */}
                     <div>
                         <h4 className='text-xl font-bold'>Company</h4>
-                        <ul className='mt-5'>
+                        <ul className='mt-2 md:mt-5'>
                             <li>About</li>
                             <li>Blogs</li>
                             <li>Careers</li>
@@ -18,7 +18,7 @@ const Footer = () => {
                     </div>
                     <div>
                         <h4 className='text-xl font-bold'>Comunity</h4>
-                        <ul className='mt-5'>
+                        <ul className='mt-2 md:mt-5'>
                             <li>Team Plans</li>
                             <li>Refer a Friend</li>
                             <li>Limited Membership</li>
@@ -28,7 +28,7 @@ const Footer = () => {
                     </div>
                     <div>
                         <h4 className='text-xl font-bold'>Importants</h4>
-                        <ul className='mt-5'>
+                        <ul className='mt-2 md:mt-5'>
                             <li>Become a Teacher</li>
                             <li>Teacher Help Center</li>
                             <li>Terms and Conditions</li>
